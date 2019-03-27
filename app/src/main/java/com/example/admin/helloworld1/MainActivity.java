0ccef5df777f1d8718e711ceaddd9abac82f0ed2
a <1139508361@qq.com>
2019-03-27 15:35:56 +0800

    1

--- app/src/main/java/com/example/admin/helloworld1/MainActivity.java
+++ app/src/main/java/com/example/admin/helloworld1/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.admin.helloworld1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textView = (TextView) findViewById(R.id.abc);
+
+        String weatherId = "CN101040200";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "258bb6d5f98f4264aac4e581aa8b01f5";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(responseText);
+                    }
+                });
+
+            }
+
+
+        });
     }
 }
